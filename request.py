4744438451a3d3694e795c4910b31affcbac56da
EliteusGalker <141252678+EliteusGalker@users.noreply.github.com>
2023-08-23 16:19:10 +0200

    Add files via upload

--- request.py
+++ request.py
@@ -1,56 +1,26 @@
-import websocket
-import threading
-import json
-import requests
+import socketio
 
-# Define the URL for the POST request
-post_url = "http://127.0.0.1:5000/waitingRoom"
+sio = socketio.Client()
 
-# Define the POST data
-post_data = {
-    'AuthKey': 'xd12345'
-}
+@sio.on('message')
+def on_message(data):
+    name = data.get('name', 'Unknown')
+    message = data.get('message', '')
+    print(f"{name}: {message}")
 
-# Send the POST request
-response = requests.post(post_url, data=post_data)
+@sio.on('connect')
+def on_connect():
+    
+    
+    print('Connected to chatroom')
 
-if response.status_code == 200:
-    print("POST request was successful!")
-    print("Response Content:")
-    print(response.text)
-else:
-    print(f"POST request failed with status code {response.status_code}")
-    exit(1)
+@sio.on('disconnect')
+def on_disconnect():
+    print('Disconnected from chatroom')
 
-# Define the WebSocket URL
-websocket_url = "ws://127.0.0.1:5000"
+session_variables = {'name': 'Bartek', 'room': '4441'}
 
-def on_message(ws, message):
-    # Define how to handle incoming WebSocket messages here
-    data = json.loads(message)
-    print(f"Received message: {data}")
+# Replace 'https://chatroom-website-url.com' with the actual URL of the chatroom
+sio.connect('http://127.0.0.1:5000/waitingRoom', headers=session_variables)
 
-def on_error(ws, error):
-    print(f"WebSocket error: {error}")
-
-def on_close(ws, close_status_code, close_msg):
-    print("WebSocket closed")
-
-def on_open(ws):
-    # Define what to do when the WebSocket connection is opened
-    print("WebSocket opened")
-
-if __name__ == "__main__":
-    # Create a WebSocket client instance
-    ws = websocket.WebSocketApp(websocket_url, on_message=on_message, on_error=on_error, on_close=on_close)
-    ws.on_open = on_open
-
-    # Start a thread to run the WebSocket client in the background
-    ws_thread = threading.Thread(target=ws.run_forever)
-    ws_thread.daemon = True  # This allows the script to exit while the WebSocket thread continues running
-    ws_thread.start()
-
-    # Your script can continue to do other tasks here while WebSocket messages are received in the background
-
-    # To gracefully close the WebSocket connection when done, you can call:
-    # ws.close()
+sio.wait()
